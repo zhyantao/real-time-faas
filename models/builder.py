a83463dc994f32c3866f0ce081380c4e39c4c3cc
zhyantao <zh6tao@gmail.com>
2022-06-18 23:47:42 +0800

    成功测试 GPU 模型并行训练

--- models/builder.py
+++ models/builder.py
@@ -1,7 +1,9 @@
+from models.ModelParallelTest import model_parallel_test
 from models.MultiCudaTest import multi_cuda_test
 from models.SingleCudaTest import single_cuda_test
 
 
 def build_model():
     # single_cuda_test()
-    multi_cuda_test()
+    # multi_cuda_test()
+    model_parallel_test()
