c240b53994f078831550a611bf881ba250ab5634
zhyantao <zh6tao@gmail.com>
2022-06-18 23:25:29 +0800

    成功测试 GPU 多卡训练

--- models/builder.py
+++ models/builder.py
@@ -1,5 +1,7 @@
+from models.MultiCudaTest import multi_cuda_test
 from models.SingleCudaTest import single_cuda_test
 
 
 def build_model():
-    single_cuda_test()
+    # single_cuda_test()
+    multi_cuda_test()
