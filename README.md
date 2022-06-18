45a1c459e300e8759630f509f1bbf20e4d6be812
zhyantao <zh6tao@gmail.com>
2022-06-18 23:18:37 +0800

    成功测试 GPU 单卡训练

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 实时 FaaS 的资源调度与任务分配方法研究
 
-Windows 10 专业版 / Python 3.7 / 专用 GPU 内存 6G
+- Windows 10 专业版
+- Python 3.8
+- NVIDIA RTX 2060 专用 GPU 内存 6G
 
--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from utils import visualization, submit, build_dataset
 from models.builder import build_model
 
-
-def main():
-    build_dataset()
-    build_model()
-    visualization()
-    submit()
-
-
 if __name__ == '__main__':
-    main()
+    build_model()
