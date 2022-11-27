88a074d33ef74c1ecc1a1ed4f2dfa9a1b3618ffb
zhyantao <zh6tao@gmail.com>
2022-11-26 19:58:19 -0800

    成功获取 containerd 运行时支持的容器 pid

--- setup/03-get-data/03-get-execution-time.sh
+++ setup/03-get-data/03-get-execution-time.sh
@@ -8,15 +8,16 @@ kubectl get node -o wide | grep -v "NAME" | awk '{print $1,$6}' >hosts
 timestamp=$(date +%s.%N)
 
 cat hosts | while read n; do
-  echo "handling ------- $n"
+  echo "handling -- $n"
   hostname=$(echo $n | awk '{print $1}')
   ip=$(echo $n | awk '{print $2}')
   echo $hostname' - '$ip' - '$event
   for pod_name in $(kubectl get pod -o wide -n $namespace | grep $hostname | awk '{print $1}'); do
-    echo 'pod_name' $pod_name ' in node' $hostname 2>&1 &
-    cmd='bash get_container_pid.sh '$pod_name
-    pid=$(echo $cmd | tr -cd "[0-9]" 2>&1 &)
-    echo $pod_name' --- pid '$pid
+    echo 'pod_name' $pod_name ' in node' $hostname 2>&1
+    url=$(kubectl describe pods -n openfaas-fn $pod_name | grep 'containerd://' | awk '{print $3}')
+    cid=${url#*//}
+    pid=$(ctr task ls | grep $cid | awk '{print $2}')
+    echo $pod_name'-- pid#'$pid
     perf stat -e $event -I $interval -p $pid -o "./tmp/"$hostname'_'$pod_name'_'$timestamp".csv" 2>&1 &
   done
 done
