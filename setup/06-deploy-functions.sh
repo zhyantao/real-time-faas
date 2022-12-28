204b11547eaf4418f86fcd78885d55fb6e4455dc
zhyantao <zh6tao@gmail.com>
2022-12-28 01:42:21 -0800

    修复 OpenWhisk 函数的路径访问错误

--- setup/06-deploy-functions.sh
+++ setup/06-deploy-functions.sh
@@ -1,6 +1,6 @@
 # Deploy OpenFaaS function
 OpenFaaS_FUNC_PATH='/root/real-time-faas/functions/openfaas'
-OpenWhisk_FUNC_PATH='/root/real-time-faas/functions/openfaas'
+OpenWhisk_FUNC_PATH='/root/real-time-faas/functions/openwhisk'
 
 cd $OpenFaaS_FUNC_PATH || exit
 faas-cli build -f openfaas.yml
@@ -8,4 +8,5 @@ faas-cli push -f openfaas.yml
 faas-cli deploy -f openfaas.yml
 
 # Deploy OpenWhisk function
-wsk -i action create hello $OpenWhisk_FUNC_PATH/hello.js
+#wsk -i action create hello $OpenWhisk_FUNC_PATH/hello.js
+wsk -i action update hello $OpenWhisk_FUNC_PATH/hello.js
