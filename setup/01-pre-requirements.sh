c54450216331b35f18d8c48314dbeab6659f6ef1
zhyantao <zh6tao@gmail.com>
2022-12-29 23:49:27 +0800

    修复重新部署环境过程中遇到的问题

--- setup/01-pre-requirements.sh
+++ setup/01-pre-requirements.sh
@@ -1,3 +1,11 @@
+# 静态分配 IP 地址
+vim /etc/sysconfig/network-scripts/ifcfg-ens33
+BOOTPROTO="static"
+IPADDR="192.168.163.146"
+GATEWAY="192.168.163.2"
+DNS1="119.29.29.29"
+systemctl restart network
+
 # 每台主机的名字不同
 hostnamectl set-hostname k8s-master  # 在主机 1 上操作
 hostnamectl set-hostname k8s-worker1 # 在主机 2 上操作
@@ -10,7 +18,8 @@ yum --enablerepo="elrepo-kernel" -y install kernel-lt.x86_64
 grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
 reboot
-yum remove $(rpm -qa | grep kernel | grep -v $(uname -r)) # 删除不需要的内核
+# （谨慎操作）删除不需要的内核
+yum remove $(rpm -qa | grep kernel | grep -v $(uname -r))
 reboot
 
 # 同步时间
@@ -23,7 +32,7 @@ sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 # 在 CentOS 7 上安装 Python 3.10 和 Git 2.38.1
 # 参考 https://docs.python.org/3/using/unix.html#custom-openssl
-# 确保存在 /etc/ssl
+# 确保下面这条命令能搜索出结果
 find /etc/ -name openssl.cnf -printf "%h\n"
 # 安装 OpenSSL
 curl -O https://www.openssl.org/source/openssl-1.1.1.tar.gz
@@ -32,25 +41,31 @@ pushd openssl-1.1.1
 ./config \
     --prefix=/usr/local \
     --libdir=lib \
-    --openssldir=/etc/ssl
+    --openssldir=$(find /etc/ -name openssl.cnf -printf "%h\n")
 make -j1 depend
 make -j8
 make install_sw
 popd
+# 安装 Python 所需的依赖
+yum update
+yum install yum-utils
+yum groupinstall "Development Tools"
+yum install bzip2-devel ncurses-devel \
+    gdbm-devel \
+    sqlite-devel tk-devel libuuid-devel \
+    readline-devel zlib-devel \
+    libpcap-devel xz-devel expat-devel libffi libffi-devel
 # 安装 Python 3.10.7
 curl -O https://www.python.org/ftp/python/3.10.7/Python-3.10.7.tgz
+tar xzf Python-3.10.7.tgz
 pushd Python-3.10.7
-./configure -C \
-    --with-openssl=/usr/local \
-    --with-openssl-rpath=auto \
-    --prefix=/usr/local
-make -j8
+./configure --enable-optimizations
 make altinstall
 popd
 # 安装 Git 2.38.1
 curl -O https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.38.1.tar.gz
 tar xzf git-2.38.1.tar.gz
-pushd git-2.28.1
+pushd git-2.38.1
 ./configure -C \
     --with-openssl=/usr/local \
     --with-openssl-rpath=auto \
