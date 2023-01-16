1e0ac41be44e173459b5ef49c244505012084c45
zhyantao <zh6tao@gmail.com>
2023-01-16 17:50:42 +0800

    进一步理解 DPE 算法

--- models/scheduler/dpe.py
+++ models/scheduler/dpe.py
@@ -1,5 +1,6 @@
 """
 Implement the DPE algorithm.
+    该算法的改进在于使用了倒数来分配流量，但是时间复杂度是极高的，只适合在简单功能下使用
 """
 import numpy as np
 import pandas as pd
@@ -52,13 +53,14 @@ class DPE:
             T_optimal = np.zeros((DAG_len, para.get_server_num()))
             start_time = np.zeros(DAG_len)
             funcs_deploy = -1 * np.ones(DAG_len)
-            process_sequence = []
+            process_sequence = []  # 记录每个 agent 上的 function 处理序列
             # server_runtime records the moment when the newest func on each server is finished
             # server_runtime 记录了最近的 function 在每个 server 上的完成时间
             server_runtime = np.zeros(para.get_server_num())
 
             makespan = 0
             for j in range(DAG_len + 1):
+                # 处理最后一个结点，收尾工作
                 if j == DAG_len:
                     # this is the dummy tail function, update all the exit functions' deployment and return the makespan
                     # makespan is the slowest 'exit function's earliest finish time'
@@ -71,8 +73,8 @@ class DPE:
                                 makespan = min(T_optimal[e])
                     break
 
-                # 如果不是最后一个结点，执行下面的代码
                 # get the number of this function and store in func_num
+                # 获取 function 编号（实际是 task 编号）
                 name_str_list = DAG.loc[j + idx, 'task_name'].strip().split('_')
                 name_str_list_len = len(name_str_list)
                 func_str_len = len(name_str_list[0])
@@ -80,14 +82,16 @@ class DPE:
 
                 if name_str_list_len == 1:
                     # func is an entry function
+                    # 入口函数
                     pass
                 else:
                     # func is not an entry function, func has dependencies
                     # enumerate the deployment of func
-                    for n in range(para.get_server_num()):
-                        # get t(p(f_j)) where p(f_j) is n
-                        process_cost = DAG_pp_required[func_num - 1] / self.pp[n]
-                        all_min_phi = []
+                    # 非入口函数，应当解决首先解决依赖
+                    for server_i in range(para.get_server_num()):
+                        # get t(p(f_j)) where p(f_j) is server_i
+                        process_cost = DAG_pp_required[func_num - 1] / self.pp[server_i]
+                        all_min_phi = []  # 记录当前服务器上的 function 所依赖的所有其他函数最早完成时间
                         for i in range(name_str_list_len - 1):
                             if name_str_list[i + 1] == '':
                                 continue
@@ -103,23 +107,26 @@ class DPE:
                                 # know that if we process (M2, R5) firstly, whether the makespan can be decreased further.
                                 # =================================================================
                                 where_deployed = int(funcs_deploy[dependent_func_num - 1])
-                                if n == funcs_deploy[dependent_func_num - 1]:
+                                if server_i == funcs_deploy[dependent_func_num - 1]:
                                     trans_cost = 0
                                 else:
-                                    trans_cost = self.proportions_list[where_deployed][n] * \
+                                    trans_cost = self.proportions_list[where_deployed][server_i] * \
                                                  DAG_data_stream[dependent_func_num - 1] * \
-                                                 self.reciprocals_list[where_deployed][n][0]
+                                                 self.reciprocals_list[where_deployed][server_i][0]
                                 min_phi = T_optimal[dependent_func_num - 1][where_deployed] + trans_cost + process_cost
                                 all_min_phi.append(min_phi)
                                 continue
 
+                            # 如果 dependent_func_num 没有部署过，应当检查 dependent_func 依赖的其他函数是否需要部署
                             for h in range(DAG_len):
                                 name_str_list_inner = DAG.loc[h + idx, 'task_name'].strip().split('_')
                                 func_str_inner_len = len(name_str_list_inner[0])
+                                # 用 if-else 找到 dependent_func 依赖的 function
                                 if int(name_str_list_inner[0][1:func_str_inner_len]) != dependent_func_num:
                                     continue
                                 else:
                                     # dependent_func_num is found
+                                    # 找到了 dependent_func_num
                                     name_str_list_inner_len = len(name_str_list_inner)
                                     if name_str_list_inner_len == 1:
                                         # dependent_func_num is an entry function. Set its T_optimal
@@ -127,7 +134,8 @@ class DPE:
                                             DAG_pp_required[dependent_func_num - 1] / self.pp + server_runtime
                                     else:
                                         # although T_optimal of dependent_func_num has been set, but it has to be
-                                        # updated because server_runtime may changed!!!
+                                        # updated because server_runtime may be changed!!!
+                                        # 虽然 dependent_func_num 的 T_optimal 已经被设置了，但是应该更新一下 server_runtime
                                         process_begin_time = np.zeros(para.get_server_num())
                                         for k in range(para.get_server_num()):
                                             min_process_begin_time = 0
@@ -163,12 +171,12 @@ class DPE:
                             min_phi = MAX_VALUE
                             selected_server = -1
                             for m in range(para.get_server_num()):
-                                if n == m:
+                                if server_i == m:
                                     trans_cost = 0
                                 else:
-                                    trans_cost = self.proportions_list[m][n] * \
+                                    trans_cost = self.proportions_list[m][server_i] * \
                                                  DAG_data_stream[dependent_func_num - 1] * \
-                                                 self.reciprocals_list[m][n][0]
+                                                 self.reciprocals_list[m][server_i][0]
                                 phi = T_optimal[dependent_func_num - 1][m] + trans_cost + process_cost
                                 if phi < min_phi:
                                     min_phi = phi
@@ -183,13 +191,13 @@ class DPE:
                             all_min_phi.append(min_phi)
 
                         # now, all the predecessors of func has been deployed, use their T_optimal to update T_optimal of func
-                        T_optimal[func_num - 1][n] = max(all_min_phi)
+                        T_optimal[func_num - 1][server_i] = max(all_min_phi)
 
-            makespan_of_all_DAGs += makespan
-            DAGs_deploy.append(funcs_deploy)
+            makespan_of_all_DAGs += makespan  # 累计 2119 个 DAG 的 makespan
+            DAGs_deploy.append(funcs_deploy)  # 每部署完一个 DAG，将其 append 到 DAGs_deploy 中
             process_sequence_all.append(process_sequence)
-            T_optimal_all.append(T_optimal)
-            start_time_all.append(start_time)
+            T_optimal_all.append(T_optimal)  # 记录所有 DAG 的最早完成时间
+            start_time_all.append(start_time)  # 记录所有所有 DAG 的最早开始时间
 
             calculated_num += 1
             percent = calculated_num / float(all_DAG_num) * 100
