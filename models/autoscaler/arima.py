749dce07ac9b3c96228622f2b781f4dbe76492ce
zhyantao <zh6tao@gmail.com>
2023-02-15 21:58:35 +0800

    删除无用的代码

--- models/autoscaler/arima.py
+++ models/autoscaler/arima.py
@@ -1,4 +1,3 @@
-import matplotlib.dates as mdates
 from matplotlib import pyplot as plt
 from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.arima.model import ARIMA
@@ -11,29 +10,6 @@ class MyARIMA:
     def __init__(self, order, future_periods):
         self.future_periods = future_periods
         self.order = order
-
-    def gauss_compare(self, original_series, predictions, data_split):
-        # the train/test split used to generate the Gaussian-filtered predictions
-        size = int(len(original_series) * data_split)
-
-        # creating a plot of the original series and Gaussian-filtered predictions
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        myFmt = mdates.DateFormatter('%m/%y')
-        ax.xaxis.set_major_formatter(myFmt)
-
-        plt.plot(original_series[size:])
-        plt.plot(predictions, color='red')
-        plt.title('Gauss-Filtered Predictions vs. Original Series')
-        plt.show()
-
-        # calculating the RMSE between the Gaussian-filtered predictions and original dataset.
-        # the +1 exception code is required when differencing is performed, as the earliest data point can be lost
-        try:
-            error = np.sqrt(mean_squared_error(predictions, original_series[size:]))
-        except:
-            error = np.sqrt(mean_squared_error(predictions, original_series[size + 1:]))
-        print('Test RMSE: %.3f' % error)
 
     def predict(self, selected_container_usage_path=para.get("selected_container_usage_path")):
         """
