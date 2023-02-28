7225a505589b3b6b38a8478c3e554f9cfba35584
zhyantao <zh6tao@gmail.com>
2023-02-28 23:00:10 +0800

    修改 OpenFaaS 中的函数调用规则

--- functions/build.sh
+++ functions/build.sh
@@ -1,6 +1,6 @@
 # Deploy OpenFaaS function
-OpenFaaS_FUNC_PATH='/root/real-time-faas/functions/openfaas'
-OpenWhisk_FUNC_PATH='/root/real-time-faas/functions/openwhisk'
+OpenFaaS_FUNC_PATH='/home/user02/real-time-faas/functions/openfaas'
+OpenWhisk_FUNC_PATH='/home/user02/real-time-faas/functions/openwhisk'
 
 cd $OpenFaaS_FUNC_PATH || exit
 faas-cli build -f openfaas.yml
--- setup/06-deploy-functions.sh
+++ setup/06-deploy-functions.sh
@@ -3,9 +3,7 @@ OpenFaaS_FUNC_PATH='/root/real-time-faas/functions/openfaas'
 OpenWhisk_FUNC_PATH='/root/real-time-faas/functions/openwhisk'
 
 cd $OpenFaaS_FUNC_PATH || exit
-faas-cli build -f openfaas.yml
-faas-cli push -f openfaas.yml
-faas-cli deploy -f openfaas.yml
+faas-cli up -f openfaas.yml
 
 # Deploy OpenWhisk function
 #wsk -i action create hello $OpenWhisk_FUNC_PATH/hello.js
