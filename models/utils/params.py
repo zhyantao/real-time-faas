7834f42ba0c0f507c60dc2d834ba437244823551
zhyantao <zh6tao@gmail.com>
2023-04-19 23:26:25 +0800

    修改参数

--- models/utils/params.py
+++ models/utils/params.py
@@ -40,8 +40,8 @@ parser.add_argument('--pp_upper', type=int, default=14, help='已有的处理器
 parser.add_argument('--pp_lower', type=int, default=7, help='已有的处理器数量下限')
 parser.add_argument('--bw_upper', type=int, default=70, help='已有的带宽总量上限')
 parser.add_argument('--bw_lower', type=int, default=30, help='已有的带宽总量下限')
-parser.add_argument('--density', type=int, default=10, help='节点的最大连接密度')
+parser.add_argument('--density', type=int, default=5, help='节点的最大连接密度')
 parser.add_argument('--n_pairs', type=int, default=12, help='？？？')
-parser.add_argument('--cpu_nums', type=int, default=20, help='处理器数量')
+parser.add_argument('--cpu_nums', type=int, default=10, help='处理器数量')
 
 args = parser.parse_args(args=[])
